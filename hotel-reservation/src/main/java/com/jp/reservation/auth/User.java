8037d9ec8101b212380ab914dd74a88b2c64aed6
Jigar Patel <44246251+jigarp4tel@users.noreply.github.com>
2020-04-04 20:02:12 -0500

    Added Authentication using Spring Security

---
+++ hotel-reservation/src/main/java/com/jp/reservation/auth/User.java
@@ -0,0 +1,47 @@
+package com.jp.reservation.auth;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "USERS")
+public class User {
+
+	@Id
+	@Column(name = "USER_ID")
+	private long user_id;
+	
+	@Column(name = "USERNAME", nullable = false, unique = true)
+	private String username;
+	
+	@Column(name = "PASSWORD")
+	private String password;
+
+	public long getUser_id() {
+		return user_id;
+	}
+
+	public void setUser_id(long user_id) {
+		this.user_id = user_id;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
+	
+}
